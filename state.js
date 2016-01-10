96a797db3e85171a31be5e3cbd12f00b060046e9
Alexander Noriega <lambdatoast@gmail.com>
2016-01-10 17:21:23 -0300

    Extract FFree into a separate module.

--- state.js
+++ state.js
@@ -4,6 +4,11 @@
 
 // http://okmij.org/ftp/Computation/free-monad.html
 
+var FFree = require('./ffree');
+var etaF = FFree.etaF;
+var fpure = FFree.fpure;
+var fimpure = FFree.fimpure;
+
 /*:: type State<s,a> = { unState: (s: s) => [a, s]; } */
 
 function state/*::<s,a>*/(f /*: (s: s) => [a, s] */) /*: State<s,a>*/ {
@@ -20,59 +25,6 @@ function runState/*::<s,a>*/(st /*: State<s,a>*/) {
   return function (s) { 
     return st.unState(s); 
   } 
-}
-
-/*:: type FFree<a> = 
-       { kind: "fpure", value: a, map: Function, chain: Function } |
-       { kind: "fimpure", gx: Object, f: Function, map: Function, chain: Function }
- */
-
-function fpure/*::<a>*/(a /*: a*/) /*: FFree<a> */ { 
-  return { 
-    value: a,
-    // Functor
-    map: function (f) { return fpure(f(a)); },
-    // Monad
-    chain: function (k) { return k(a); },
-    // Ugh
-    kind: 'fpure'
-  } 
-}
-
-function fimpure(gx) { 
-  return function (f /*: Function */) { 
-    return { 
-      gx: gx, 
-      f: f,
-      // Functor
-      map: function (f_) { 
-        return fimpure(gx)(function (x) {
-          return f(x).map(f_);
-        });
-      },
-      // Monad
-      // FImpure gx f >>= k = FImpure gx (f >>> k)
-      chain: function (k) {
-        return fimpure(gx)(kompose(f)(k));
-      },
-      // Ugh
-      kind: 'fimpure'
-    }; 
-  } 
-}
-
-// (>>>) :: Monad m => (a -> m b) -> (b -> m c) -> (a -> m c)
-// f >>> g = (>>= g) . f
-function kompose(f) {
-  return function (g) {
-    return function (a) { 
-      return f(a).chain(g);
-    };
-  };
-}
-
-function etaF/*::<a>*/(fa) /*: FFree<a> */ {
-  return fimpure(fa)(fpure);
 }
 
 function compose(g) { 
