80188e3f571c7763f68377e86b0b2d0b5ee3b071
Alexander Noriega <lambdatoast@gmail.com>
2016-01-10 23:53:04 -0300

    Remove unnecessary imports.

--- eff-geomap.js
+++ eff-geomap.js
@@ -3,10 +3,7 @@
  */
 
 var R = require('ramda');
-var FFree = require('./ffree');
-var etaF = FFree.etaF;
-var fpure = FFree.fpure;
-var fimpure = FFree.fimpure;
+var etaF = require('./ffree').etaF;
 
 /*:: type GeoMap<a> = 
        { kind: "AddMarker", value: a} |
@@ -18,10 +15,10 @@ var fimpure = FFree.fimpure;
 function AddMarker(a) { return { kind: "AddMarker", a: a }; }
 function RemoveMarker(a) { return { kind: "RemoveMarker", a: a }; }
 
-var addMarker = R.compose(etaF,AddMarker);
-var removeMarker = R.compose(etaF,RemoveMarker);
+var addMarker = R.compose(etaF, AddMarker);
+var removeMarker = R.compose(etaF, RemoveMarker);
 
 module.exports = {
   addMarker: addMarker,
   removeMarker: removeMarker
-}
+};
